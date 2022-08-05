5ae8360ac0ce6a5ce3f722dbb4177ba928802b5f
RashadXalil <rashadmkh@code.edu.az>
2022-08-05 12:37:36 +0400

    Lab

---
+++ 08-04-2022/08-04-2022/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _08_04_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Task-1
+            //int sum = 0;
+            //Console.Write("ededlerin sayini daxil edin: ");
+            //string strCount = Console.ReadLine();
+            //int intCount = int.Parse(strCount);
+            //Console.WriteLine(intCount);
+            //for (int i = 0; i < intCount; i++)
+            //{
+            //    Console.Write("Enter Number :");
+            //    string strInp = Console.ReadLine();
+            //    int intInp = int.Parse(strInp);
+            //    sum += intInp;
+            //}
+            //Console.WriteLine("Sum : " + sum / intCount);
+            #endregion
+            #region Task-2
+            //Console.WriteLine("Number : ");
+            //string strInput = Console.ReadLine();
+            //int intInput = int.Parse(strInput);
+            //if (intInput == 0)
+            //{
+            //    Console.WriteLine("ne tek ne cut");
+            //}
+            //else if (intInput % 2 == 0)
+            //    Console.WriteLine("cut");
+            //else
+            //    Console.WriteLine("tek");
+
+            #endregion
+            #region Task-3
+            //Console.Write("Enter a: ");
+            //string str_a = Console.ReadLine();
+            //int int_a = int.Parse(str_a);
+            //Console.Write("Enter B : ");
+            //string str_b = Console.ReadLine();
+            //int int_b = int.Parse(str_b);
+
+            //double p = 2 * (int_a + int_b);
+            //Console.WriteLine(p);
+            #endregion Tree
+            //int spaces = 10;
+            //int asterix = 1;
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < spaces; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int j = 0; j < asterix; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+            //    asterix++;
+            //    spaces--;
+            //}
+
+
+        }
+    }
+}
