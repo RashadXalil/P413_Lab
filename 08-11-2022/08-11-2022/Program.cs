579a86ef9e81043bc6a4f11510059a4917c8777e
RashadXalil <rashadmkh@code.edu.az>
2022-08-11 21:51:57 +0400

    lab

---
+++ 08-11-2022/08-11-2022/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace _08_11_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2 eded goturursuz
+            // */+/-/{/}
+            // mebleg muddet faiz
+            // aylig odenis umumi odenilecek mebleg
+            // 1000 14% 95 1140
+            //string a = "12";
+            //string b = "13";
+            //Console.WriteLine(a+b);
+            //Console.Write("Enter a : ");
+            //string str_a = Console.ReadLine();
+            //int int_a = int.Parse(str_a);
+            //Console.Write("Enter b : ");
+            //string str_b = Console.ReadLine();
+            //int int_b = int.Parse(str_b);
+            //Console.WriteLine("1.+    2.-    3.*    4./");
+            //Console.Write("Choose :");
+            //string str_op = Console.ReadLine();
+            //switch (str_op)
+            //{
+            //    case "1":
+            //        Console.WriteLine(Toplama(int_a, int_b));
+            //        break;
+            //    case "2":
+            //        Console.WriteLine(Cixma(int_a, int_b));
+            //        break;
+            //    case "3":
+            //        Console.WriteLine(Vurma(int_a, int_b));
+            //        break;
+            //    case "4":
+            //        Console.WriteLine(Bolme(int_a, int_b));
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+            // 1.Meblegi goturmek
+            //2.Bank faizi yazmag
+            //    3.muddet yazmag
+            //    4.meblegi faize vurub muddete bolmek
+            //    5.data geri gayitmag
+
+            //Console.WriteLine("meblegi daxil edin");
+            //string str_a = Console.ReadLine();
+            //Console.WriteLine("Faizi daxil edin:");
+            //string str_b = Console.ReadLine();
+            //Console.WriteLine("Muddeti daxil edin:");
+            //string str_c = Console.ReadLine();
+            //int a = int.Parse(str_a);
+            //int b = int.Parse(str_b);
+            //int c = int.Parse(str_c);
+            //int d = (a * b) / 100 + a;
+            //int e = (d / c);
+            //Console.WriteLine("Umumi mebleg " + d + "Ayliq odenis" + e);
+
+            // array gotururem
+            // loopa saliram
+            // deyeri log etmek
+
+            // array yaratmaq
+            // axtarilacaq ededi userden gotururuk
+            // true false qaytaririq
+            //int[] arr = new int[5] { 1,2,3,4,5};
+            //Console.Write("Enter Number : ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //string a = "12";
+            // convert,parse,tryparse
+
+            //int b = int.Parse(a);
+            //Console.WriteLine(b.GetType());
+            //int c = Convert.ToInt32(a);
+            //Console.WriteLine(c.GetType());
+
+            //bool res;
+            //string str = "11";
+            //int int_a;
+            //res=int.TryParse(str, out int_a);
+            //Console.WriteLine(res);
+            //Console.WriteLine(int_a);
+
+            //int sum = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    sum += arr[i];
+            //}
+            //Console.WriteLine("Sum : "+sum);
+            //decimal int string float 
+        }
+        //public static int Toplama(int a, int b)
+        //{
+        //    return a + b;
+        //}
+        //public static int Cixma(int a, int b)
+        //{
+        //    if (a > b)
+        //        return a - b;
+        //    else 
+        //        return b - a;
+        //}
+        //public static int Vurma(int a ,int b)
+        //{
+        //    return a * b;
+        //}
+        //public static int Bolme(int a ,int b)
+        //{
+        //    if (a > b)
+        //    {
+        //        if (b == 0)
+        //        {
+        //            Console.WriteLine("0-a bolmek olmaz");
+        //            return 0;
+        //        }
+        //        return a / b;
+        //    }
+        //    else
+        //    {
+        //        return b / a;
+        //    }
+
+        //}
+        //public static bool Allahsukur(int[] arr,int target)
+        //{
+        //    bool res = false;
+        //    // 1 2 3 4 5
+        //    // 7
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] == target)
+        //        {
+        //            res = true;                 
+        //        }
+        //    }
+        //    return res;
+        //}
+    }
+
+}
